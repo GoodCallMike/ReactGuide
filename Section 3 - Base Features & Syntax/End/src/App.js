18927386a39eaac36d622870ce63c361fffb7204
Michael Saylor <saylormic@gmail.com>
2020-02-05 21:50:06 -0500

    Fix Style Issues

--- Section 3 - Base Features & Syntax/End/src/App.js
+++ Section 3 - Base Features & Syntax/End/src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import "./App.css";
-import Person from "./Person/Person";
+import React from 'react';
+import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
