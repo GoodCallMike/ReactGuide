27c251cdb60fa0dd81ddfd1cb9f26bf72c6f9e15
Michael Saylor <saylormic@gmail.com>
2020-02-05 21:45:13 -0500

    Complete Lecture 37
    Understanding the "children" prop

--- Section 3 - Base Features & Syntax/End/src/App.js
+++ Section 3 - Base Features & Syntax/End/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 function App() {
   return (
@@ -8,7 +8,9 @@ function App() {
       <h1>Hi, I'm a React App</h1>
       <p>This is really working!</p>
       <Person name="Max" age="26" />
-      <Person name="Manu" age="29" >My Hobbies: Racing</Person>
+      <Person name="Manu" age="29">
+        My Hobbies: Racing
+      </Person>
       <Person name="Stephanie" age="26" />
     </div>
   );
